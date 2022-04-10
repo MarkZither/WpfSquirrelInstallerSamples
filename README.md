c63a43c91c93c34059220b96aded4b35412ae1e9
MarkZither <mark.burton@zither-it.co.uk>
2022-04-10 21:38:50 +0200

    finish renaming and first installer to get url

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # WpfSquirrelInstallerSamples
 Simple WPF applications to show different squirrel installers in action
+
+
+## How to create installation packages for Wpf.SquirrelInstaller.SampleApp.NetFullFramework
+In Package Manager Console from the `WpfSquirrelInstallerSamples\Wpf.SquirrelInstaller.SampleApp.NetFullFramework` directory run the following commands
+
+```
+.\nuget.exe pack 
+squirrel --releasify .\WpfSampleAppFullFramework.1.0.0.nupkg -r FullFramework
+```
---
+++ Wpf.SquirrelInstaller.SampleApp.NetFullFramework/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Squirrel;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2FullFramework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var updateManager = new UpdateManager(@"C:\Source\GitRepos\WpfSquirrelInstallerSamples\Wpf.SquirrelInstaller.SampleApp.NetFullFramework"))
+                {
+                    CurrentVersion.Content = $"Current version: {updateManager.CurrentlyInstalledVersion()}";
+                    var releaseEntry = await updateManager.UpdateApp();
+                    NewVersion.Content = $"Update Version: {releaseEntry?.Version.ToString() ?? "No update"}";
+                }
+            }
+            catch(Exception ex)
+            {
+                CurrentVersion.Content = "Not a squirrel install";
+            }
+        }
+    }
+}
