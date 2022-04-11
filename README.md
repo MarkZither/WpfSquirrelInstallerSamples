0c427a1b4c7d269915e851f5ba3a44f6ae534793
MarkZither <mark.burton@zither-it.co.uk>
2022-04-11 21:49:50 +0200

    Add Clowd.Squirrel to .net6 example

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ In Package Manager Console from the `WpfSquirrelInstallerSamples\Wpf.SquirrelIns
 .\nuget.exe pack 
 squirrel --releasify .\WpfSampleAppFullFramework.1.0.0.nupkg -r FullFramework
 ```
+
+
+## How to create installation packages for Wpf.SquirrelInstaller.SampleApp.Net6
+In Package Manager Console from the `WpfSquirrelInstallerSamples\Wpf.SquirrelInstaller.SampleApp.Net6` directory run the following commands
+
+```
+Set-Alias ClowdSquirrel ($env:USERPROFILE + "\.nuget\packages\clowd.squirrel\2.8.40\tools\Squirrel.exe");
+# build / publish your app, csproj settings make this a self-contained single file application
+dotnet publish -c Release -o ".\publish" 
+ClowdSquirrel pack --packId "YourApp" --packVersion "1.0.0" --packAuthors "Mark Burton" --packDirectory ".\publish"
+```
--- Wpf.SquirrelInstaller.SampleApp.Net6/App.xaml.cs
+++ Wpf.SquirrelInstaller.SampleApp.Net6/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Squirrel;
+
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,32 @@ namespace WpfApp1
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // run Squirrel first, as the app may exit after these run
+            SquirrelAwareApp.HandleEvents(
+                onInitialInstall: OnAppInstall,
+                onAppUninstall: OnAppUninstall,
+                onEveryRun: OnAppRun);
+
+            base.OnStartup(e);
+        }
+
+        private static void OnAppInstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.CreateShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+        }
+
+        private static void OnAppUninstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.RemoveShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+        }
+
+        private static void OnAppRun(SemanticVersion version, IAppTools tools, bool firstRun)
+        {
+            tools.SetProcessAppUserModelId();
+            // show a welcome message when the app is first installed
+            if (firstRun) MessageBox.Show("Thanks for installing my application!");
+        }
     }
 }
