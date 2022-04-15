7d60dceba0413927eec4839514a0679ac5c90ee0
MarkZither <mark.burton@zither-it.co.uk>
2022-04-15 22:08:17 +0200

    Add project to compare single file self-contained vs framework-dependent

---
+++ Wpf.SquirrelInstaller.SampleApp.Net6.FrameworkDependent/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wpf.SquirrelInstaller.SampleApp.Net6.FrameworkDependent
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
